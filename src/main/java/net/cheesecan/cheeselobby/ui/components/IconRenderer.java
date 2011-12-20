fbbd2a42dbcfbf7baa41eb25613a30d7d6667302
Jahziah <jahziah@kth.se>
2011-12-20 13:26:54 +0100

    Made license terms clearer.

--- src/main/java/net/cheesecan/cheeselobby/ui/components/IconRenderer.java
+++ src/main/java/net/cheesecan/cheeselobby/ui/components/IconRenderer.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010 jahziah.
+ *  Copyright 2010 Jahziah Wagner <jahziah[dot]wagner[at]gmail.com>.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
