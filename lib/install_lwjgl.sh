ec7e557161c041598df8eb831c4fae728e1962af
Jahziah <jahziah@kth.se>
2011-12-20 13:32:09 +0100

    Removed lwjgl which is now available in maven repo

--- lib/install_lwjgl.sh
+++
@@ -1,3 +0,0 @@
-mvn install:install-file -Dfile=./lwjgl.jar -DgroupId=org.lwjgl -DartifactId=lwjgl -Dversion=2.6 -Dpackaging=jar
-mvn install:install-file -Dfile=./jinput.jar -DgroupId=org.lwjgl -DartifactId=jinput -Dversion=2.6 -Dpackaging=jar
-mvn install:install-file -Dfile=./lwjgl_util.jar -DgroupId=org.lwjgl -DartifactId=lwjgl-util -Dversion=2.6 -Dpackaging=jar
